cd5914a266e10ddc69a162b3206b5e1e5913604d
Andrew Dymond <andy.dymond@gmail.com>
2018-02-02 16:02:04 -0700

    Add articles index template

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -1,17 +1,15 @@
 require "rails_helper"
 
 describe "user sees all articles" do
-  describe "the visit /articles" do
+  describe "they visit /articles" do
     it "displays all articles" do
-      article_1 = Article.new(title:"title 1", body: "body 1")
-      article_2 = Article.new(title:"title 2", body: "body 2")
+      article_1 = Article.create(title: "Title 1", body: "Body 1")
+      article_2 = Article.create(title: "Title 2", body: "Body 2")
 
-      visit "/articles"
+      visit '/articles'
 
       expect(page).to have_content(article_1.title)
-      expect(page).to have_content(article_1.body)
       expect(page).to have_content(article_2.title)
-      expect(page).to have_content(article_2.body)
     end
   end
 end
