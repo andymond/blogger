83844a93de0246adebfd3752db661f4cd28ce331
Andrew Dymond <andy.dymond@gmail.com>
2018-02-03 15:50:32 -0700

    Refactor edit & new views by pulling form to partial

--- spec/features/user_can_edit_article_spec.rb
+++ spec/features/user_can_edit_article_spec.rb
@@ -12,7 +12,7 @@ describe "User can edit article" do
       expect(current_path).to eq(edit_article_path(article_1))
 
       fill_in "article[title]", with: "New Title!"
-      click_on "Submit Changes"
+      click_on "Update Article"
 
       expect(current_path).to eq(article_path(article_1))
       expect(page).to have_content("New Title!")
