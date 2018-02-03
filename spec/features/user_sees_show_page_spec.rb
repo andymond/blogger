38593fe1bcb63a7b0da5d8f29725ab939ec50305
Andrew Dymond <andy.dymond@gmail.com>
2018-02-03 16:47:29 -0700

    Add comments to show view and create partial _comment

--- spec/features/user_sees_show_page_spec.rb
+++ spec/features/user_sees_show_page_spec.rb
@@ -4,6 +4,8 @@ describe "User navigates to article show page" do
   describe "User navigates to article from index" do
     it "displays that article's information" do
       article = Article.create(title: "How to shave a mermaid", body: "Don't do it!")
+      comment_1 = article.comments.create(authorname: "Ash", body: "All Pokemon are great")
+      comment_2 = article.comments.create(authorname: "Misty", body: "Only Water Pokemon are great")
 
       visit articles_path
       click_link article.title
@@ -11,6 +13,10 @@ describe "User navigates to article show page" do
       expect(current_path).to eq("/articles/#{article.id}")
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
+      expect(page).to have_content(comment_1.authorname)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.authorname)
+      expect(page).to have_content(comment_2.body)
     end
   end
 end
