47ca5e247c77889d1f2c62386f4f9008bc0d18aa
Andrew Dymond <andy.dymond@gmail.com>
2018-02-03 19:28:50 -0700

    Add relationships to tag, article & tagging models

--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe Tagging type: :model do
+describe Tagging, type: :model do
   describe "Relationships" do
     it {should belong_to :tag}
     it {should belong_to :article}
