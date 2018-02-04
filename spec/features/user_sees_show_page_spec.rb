47bf4b1424552a70e7817aa58cdbc29e1213456b
Andrew Dymond <andy.dymond@gmail.com>
2018-02-03 18:32:19 -0700

    Update article show page spec to test for comments and add comment resources to routes config

--- spec/features/user_sees_show_page_spec.rb
+++ spec/features/user_sees_show_page_spec.rb
@@ -19,4 +19,21 @@ describe "User navigates to article show page" do
       expect(page).to have_content(comment_2.body)
     end
   end
+
+  describe "User fills in comment form" do
+    it "displays comment on article show page" do
+      article = Article.create(title: "How to be cool", body: "Keep your temperature about 10 degress under room temperature")
+
+      visit article_path(article)
+
+      fill_in "comment[authorname]", with: "ME!"
+      fill_in "comment[body]", with: "Sounds like a bad idea!"
+      click_on "Submit"
+
+      expect(current_path).to eq(article_path(article))
+      expect(page).to have_content("Post a Comment")
+      expect(page).to have_content("ME!")
+      expect(page).to have_content("Sounds like a bad idea!")
+    end
+  end
 end
